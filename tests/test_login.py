4c33a48363f52575b4272c1982af6f78ff73c7d6
Aleksandra Novichkova <latheralex@yandex.ru>
2026-09-15 22:29:42 +0300

    исправление теста по авторизации

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,26 @@
-from locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import MainPageLocators, LoginPageLocators
 
 
 class TestLogin:
 
-    def test_login_success(self, authorized_user):
+    def test_login_success(self, registered_user):
     #   login пользователя
-        assert authorized_user.find_element(*MainPageLocators.USER_NAME).text == "User."
+        driver = registered_user["driver"]
+
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable(MainPageLocators.LOGIN_BUTTON)
+        ).click()
+
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(LoginPageLocators.LOGIN_EMAIL_INPUT)
+        ).send_keys(registered_user["email"])
+
+        driver.find_element(*LoginPageLocators.LOGIN_PASSWORD_INPUT).send_keys(registered_user["password"])
+        driver.find_element(*LoginPageLocators.LOGIN_SUBMIT_BUTTON).click()
+
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.USER_AVATAR)
+        )
+        assert driver.find_element(*MainPageLocators.USER_NAME).text == "User."

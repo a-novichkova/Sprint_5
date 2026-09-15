ba8725c8b07fd787aaaea40b7abb96556388e28b
Aleksandra Novichkova <latheralex@yandex.ru>
2026-09-15 22:46:45 +0300

    исправление test_registration.py: переписана регистрация пользователя

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,5 @@
 import random
 import string
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainPageLocators, RegistrationPageLocators
@@ -46,31 +45,9 @@ class TestRegistration:
         assert driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD_ERROR).is_displayed()
         assert driver.find_element(*RegistrationPageLocators.CONFIRM_FIELD_ERROR).is_displayed()
 
-    def test_registration_existing_user(self, driver):
+    def test_registration_existing_user(self, registered_user):
     #   регистрация уже существующего пользователя
-        email = f"test_{''.join(random.choices(string.ascii_lowercase + string.digits, k=8))}@example.com"
-        password = "ValidPass123"
-
-        # создание нового пользователя с уникальным email
-        driver.find_element(*MainPageLocators.LOGIN_BUTTON).click()
-        WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable(RegistrationPageLocators.REGISTER_LINK)
-        ).click()
-
-        driver.find_element(*RegistrationPageLocators.EMAIL_INPUT).send_keys(email)
-        driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT).send_keys(password)
-        driver.find_element(*RegistrationPageLocators.CONFIRM_PASSWORD_INPUT).send_keys(password)
-        driver.find_element(*RegistrationPageLocators.CREATE_BUTTON).click()
-
-        WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(MainPageLocators.USER_AVATAR)
-        )
-
-        # выход из аккаунта, чтобы снова открыть форму регистрации
-        driver.find_element(*MainPageLocators.LOGOUT_BUTTON).click()
-        WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(MainPageLocators.LOGIN_BUTTON)
-        )
+        driver = registered_user["driver"]
 
         # попытка зарегистрироваться повторно с теми же данными
         driver.find_element(*MainPageLocators.LOGIN_BUTTON).click()
@@ -78,9 +55,9 @@ class TestRegistration:
             EC.element_to_be_clickable(RegistrationPageLocators.REGISTER_LINK)
         ).click()
 
-        driver.find_element(*RegistrationPageLocators.EMAIL_INPUT).send_keys(email)
-        driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT).send_keys(password)
-        driver.find_element(*RegistrationPageLocators.CONFIRM_PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*RegistrationPageLocators.EMAIL_INPUT).send_keys(registered_user["email"])
+        driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT).send_keys(registered_user["password"])
+        driver.find_element(*RegistrationPageLocators.CONFIRM_PASSWORD_INPUT).send_keys(registered_user["password"])
         driver.find_element(*RegistrationPageLocators.CREATE_BUTTON).click()
 
         # проверка, что поля выделены красным и есть сообщение об ошибке
